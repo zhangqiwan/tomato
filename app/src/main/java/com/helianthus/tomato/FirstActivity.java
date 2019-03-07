84780b987fdf23ba3112c2827e6a35e16433ae9a
zhangqiwan <1104024221@qq.com>
2019-03-07 09:40:21 +0800

    test activity about view and logic

---
+++ app/src/main/java/com/helianthus/tomato/FirstActivity.java
@@ -0,0 +1,25 @@
+package com.helianthus.tomato;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_first);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FirstActivity.this, "You clicked Button1",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
